a902d92eeee56346d3980ffb8a850d88141bdf78
Alessandro <alexdif980@gmail.com>
2023-11-01 18:58:54 +0000

    debug: 'coo_matrix' object is not subscriptable

--- cdl/data.py
+++ cdl/data.py
@@ -37,12 +37,16 @@ def load_content_embeddings(dataset_name, embedding=None, path=None, device=None
 
 def load_cf_train_data(dataset_name, path=None):
     if path is None: path = f'data/processed/{dataset_name}/cf-train-1.pt'
-    return torch.load(path)
+    mat = torch.load(path)
+    print(type(mat), mat.shape, type(mat[0]))
+    return mat
 
 
 def load_cf_test_data(dataset_name, path=None):
     if path is None: path = f'data/processed/{dataset_name}/cf-test-1.pt'
-    return torch.load(path)
+    mat = torch.load(path)
+    print(type(mat), mat.shape, type(mat[0]))
+    return mat
 
 
 def save_model(sdae, mfm, filename):
