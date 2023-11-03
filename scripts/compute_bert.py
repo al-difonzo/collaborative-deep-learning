64098193de691eda18794d05049973341589061a
Alessandro <alexdif980@gmail.com>
2023-11-03 10:11:32 +0000

    bert embedding for amz datasets + sample data

--- scripts/compute_bert.py
+++ scripts/compute_bert.py
@@ -1,29 +1,32 @@
 import sys
+import os
 
 import torch
 import pandas as pd
 from sentence_transformers import SentenceTransformer
-import path
-directory = path.path(__file__).abspath()
-sys.path.append(directory.parent.parent)
+from importlib.machinery import SourceFileLoader
+constants = SourceFileLoader("constants","cdl/constants.py").load_module()
 import constants
 
 def embed_and_save(content, path, check_for_nan=False):
     embeddings = model.encode(content, convert_to_tensor=True)
     if check_for_nan:
         assert type(content)==pd.Series, f'Not applicable to {type(content)}'
-        empty_mask = [x=='' for x in content]
-        idx_first_non_empty_elem = content[~empty_mask].index[0]
-        zero_filler = torch.zeros_like(embeddings[idx_first_non_empty_elem])
-        embeddings = embeddings.masked_fill_(empty_mask, zero_filler)
+        nan_mask = pd.isna(content)
+        if all(nan_mask): raise ValueError("Please provide an iterable with at least 1 non-empty element!")
+        mask_ = torch.tensor(nan_mask).unsqueeze(-1).expand(embeddings.size())
+        embeddings = embeddings.masked_fill_(mask_, 0)
     
+    print(f'Saving embeddings with size {embeddings.shape} to {path}')
+    os.makedirs(os.path.dirname(path), exist_ok=True)
     torch.save(embeddings, path)
     return embeddings
 
 
 if __name__ == '__main__':
     dataset_name = sys.argv[1]
-    dataset_path = sys.argv[2]
+    in_path = sys.argv[2] if len(sys.argv)>=3 else None
+    out_path = sys.argv[3] if len(sys.argv)>=4 else None
     ST_MODEL = 'allenai-specter'
     model = SentenceTransformer(ST_MODEL)
 
@@ -39,9 +42,10 @@ if __name__ == '__main__':
         embed_and_save(content, f'data/processed/{dataset_name}/content-bert.pt')
 
     elif dataset_name in constants.AMZ_CHOICES_:
-        df = pd.read_csv(dataset_path)
+        df = pd.read_csv(in_path)
         for col in constants.AMZ_EMBEDDED_COLS:
-            df_col = df[col].fillna('')
-            embed_and_save(df[col], f'data/processed/{dataset_name}/{col}_embedded_{ST_MODEL}.pt', check_for_nan=True)
+            print(f'Embedding column {col}...')
+            if out_path is None: out_path = f'data/processed/{dataset_name}/{col}_embedded_{ST_MODEL}.pt'
+            embed_and_save(df[col], out_path, check_for_nan=True)
 
 
