af548539605a21288a7bf44c6f28c302534d3d82
Alessandro <alexdif980@gmail.com>
2024-02-06 10:44:04 +0000

    refactor hyper

--- cdl/hyper.py
+++ cdl/hyper.py
@@ -37,39 +37,51 @@ class OptunaWrapper:
             'corruption': self.args.corruption,
         })
         logging.info(f'Config: {config}')
-        
+
         optimizer = torch.optim.AdamW(self.sdae.parameters(), lr=self.args.lr, weight_decay=config['lambda_w'])
 
         content_training_dataset = data.random_subset(self.content_data, int(self.num_items * 0.75))
-
-        # EPOCHS = trial.suggest_int('epochs', 5, 20)
-        EPOCHS = 2
         
         logging.info(f'Pretraining SDAE with {self.args.recon_loss} loss for {EPOCHS} epochs')
         cdl.train_stacked_autoencoder(self.sdae, content_training_dataset, self.args.corruption, EPOCHS, self.args.batch_size, self.recon_loss_fn, optimizer)
-        # cdl.train_stacked_autoencoder(self.sdae, content_training_dataset, self.args.corruption, self.args.pretrain_epochs, self.args.batch_size, self.recon_loss_fn, optimizer)
         
-        for epoch in range(EPOCHS):
-            # Train the model
-            logging.info(f'Training with recon loss {self.args.recon_loss}')
-            cdl.train_model(self.sdae, self.mfm, self.content_data, self.train_data, optimizer, self.recon_loss_fn, config, epochs=EPOCHS, batch_size=self.args.batch_size, device=self.device)
-            # Evaluate the model on the validation set
-            recall = self.mfm.compute_recall(self.valid_data.to_dense(), self.args.topk)
+        # EPOCHS = trial.suggest_int('epochs', 5, 20)
+        EPOCHS = 2
+        # Train the model
+        cdl.train_model(self.sdae, self.mfm, self.content_data, self.train_data, optimizer, self.recon_loss_fn, config, epochs=EPOCHS, batch_size=self.args.batch_size, device=self.device)
+        recall = self.mfm.compute_recall(self.valid_data.to_dense(), self.args.topk)
+        trial.report(recall, 0)
+        
+        # for epoch in range(EPOCHS):
+        #     logging.info(f'Training with recon loss {self.args.recon_loss}')
+        #     # Evaluate the model on validation set
+        #     recall = self.mfm.compute_recall(self.valid_data.to_dense(), self.args.topk)
 
-            trial.report(recall, epoch)
+        #     trial.report(recall, epoch)
 
-            # Handle pruning based on the intermediate value.
-            if trial.should_prune():
-                raise optuna.exceptions.TrialPruned()
+        #     # Handle pruning based on the intermediate value.
+        #     if trial.should_prune():
+        #         raise optuna.exceptions.TrialPruned()
 
-        return 1 - recall  # Optuna minimizes the objective function, and higher is better for recall
+        return 1 - recall  # Optuna minimizes the objective function, whereas recall should be maximized
 
     def optimize(self, n_trials=10, study_name=None):
         study = optuna.create_study(direction='minimize', study_name=study_name)
-        study.optimize(self.objective, n_trials=n_trials)
+        study.optimize(self.objective, n_trials=n_trials, timeout=1800) # timeouts after 30 minutes, if not yet stopped due to n_trials
 
-        # Print the best hyperparameters
-        best_params = study.best_params
-        logging.info("Best Hyperparameters:", best_params)
+        print("Study statistics: ")
+        print("  Number of finished trials: ", len(study.trials))
+        print("  Number of pruned trials: ", len(study.get_trials(deepcopy=False, states=[optuna.trial.TrialState.PRUNED])))
+        print("  Number of complete trials: ", len(study.get_trials(deepcopy=False, states=[optuna.trial.TrialState.COMPLETE])))
+
+        trial = study.best_trial
+        print("Best trial:")
+        print("  Value: ", trial.value)
+        print("  Params: ")
+        for key, value in trial.params.items():
+            print("    {}: {}".format(key, value))
+        # # Print the best hyperparameters
+        # best_params = study.best_params
+        # logging.info("Best Hyperparameters:", best_params)
         
         return study
