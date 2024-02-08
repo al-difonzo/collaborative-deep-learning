bc00c31f7a229e479c00226a1c9529a197a6433c
al-difonzo <a.difonzo2@studenti.poliba.it>
2024-02-08 19:17:29 +0000

    refactor infer.py to compute test recall for ALL trials, instead of only best (from Colab)

--- cdl/hyper.py
+++ cdl/hyper.py
@@ -25,10 +25,10 @@ class OptunaWrapper:
     
     def get_hyper_combo(self, trial):
         hypers = {
-            'lambda_u': trial.suggest_float("lambda_u", 1e-2, 1e2, log=True),
-            'lambda_v': trial.suggest_float("lambda_v", 1e-2, 1e2, log=True),
-            'lambda_w': trial.suggest_float("lambda_w", 1e-2, 1e2, log=True),
-            'lambda_n': trial.suggest_float("lambda_n", 1e-2, 1e2, log=True),
+            'lambda_u': trial.suggest_float("lambda_u", 2e-2, 2e1, log=True),
+            'lambda_v': trial.suggest_float("lambda_v", 2e-2, 2e1, log=True),
+            'lambda_w': trial.suggest_float("lambda_w", 2e-2, 2e1, log=True),
+            'lambda_n': trial.suggest_float("lambda_n", 2e-2, 2e1, log=True),
         }
         dangerous_ratio = hypers['lambda_n'] / hypers['lambda_v']
         return hypers, dangerous_ratio < 1e-2 or dangerous_ratio > 1e2
@@ -36,8 +36,6 @@ class OptunaWrapper:
     
     def objective(self, trial):
         config, extreme_case = self.get_hyper_combo(trial)
-        if extreme_case:
-            raise optuna.exceptions.TrialPruned()
         config.update({ # non-hyper parameters
             'conf_a': self.args.conf_a,
             'conf_b': self.args.conf_b,
@@ -45,6 +43,7 @@ class OptunaWrapper:
             'corruption': self.args.corruption,
         })
         logging.info(f'Config: {config}')
+        if extreme_case: raise optuna.exceptions.TrialPruned()
 
         optimizer = torch.optim.AdamW(self.sdae.parameters(), lr=self.args.lr, weight_decay=config['lambda_w'])
 
@@ -72,7 +71,16 @@ class OptunaWrapper:
     def optimize(self, n_trials=10, timeout=600, study_name=None, storage=None):
         study = optuna.create_study(direction='minimize', study_name=study_name, storage=storage, load_if_exists=True)
         
-        # study.enqueue_trial({"lambda_n": 0.2560861375505139, "lambda_u": 0.09906732983615665, "lambda_v": 14.116461137607097, "lambda_w": 0.031627597935146495})
+        study.enqueue_trial({'lambda_n': 0.2560861375505139, 'lambda_u': 0.0990673298361566, 'lambda_v': 14.116461137607097, 'lambda_w': 0.0316275979351464})
+        study.enqueue_trial({'lambda_n': 2.7691590625599223, 'lambda_u': 0.029149783135337, 'lambda_v': 0.0672587691009253, 'lambda_w': 5.058158379843988})
+        study.enqueue_trial({'lambda_n': 3.1112180261398668, 'lambda_u': 0.0256784335956293, 'lambda_v': 0.0664517443448319, 'lambda_w': 2.840059384124836})
+        study.enqueue_trial({'lambda_n': 1.0259320918617851, 'lambda_u': 0.0323645844154793, 'lambda_v': 0.0351464324818799, 'lambda_w': 1.7129012689457732})
+        study.enqueue_trial({'lambda_n': 0.1082215501082346, 'lambda_u': 0.399805428514618, 'lambda_v': 0.4113994116701447, 'lambda_w': 0.3388189806592133})
+        study.enqueue_trial({'lambda_n': 18.0498825981281, 'lambda_u': 0.3611354216647621, 'lambda_v': 4.954212521008054, 'lambda_w': 0.2913460121936407})
+        study.enqueue_trial({'lambda_n': 0.2098624338363367, 'lambda_u': 0.1886385462396957, 'lambda_v': 1.5581897468559742, 'lambda_w': 0.596533878625882})
+        study.enqueue_trial({'lambda_n': 0.4054331842975566, 'lambda_u': 1.265590349032221, 'lambda_v': 0.0324811970271214, 'lambda_w': 0.7219385365488191})
+        study.enqueue_trial({'lambda_n': 5.787595985786128, 'lambda_u': 0.7257817092577302, 'lambda_v': 0.1657546218249667, 'lambda_w': 0.7008189802215785})
+        study.enqueue_trial({'lambda_n': 0.4553469590812619, 'lambda_u': 2.993619725182766, 'lambda_v': 0.6993915394948664, 'lambda_w': 4.02489957495183})
         
         study.optimize(self.objective, n_trials=n_trials, timeout=timeout)
         
@@ -104,15 +112,15 @@ class OptunaWrapper:
         # print(study_df)
         # study_df.to_csv(study_df_path)
 
-        logging.info('Cleaning artifacts from non-best trials')
-        trials_parent_dir = os.path.dirname(self.args.model_path)
-        best_trial_folder = f'trial_{study.best_trial.number}'
-        dirs_to_clean = [d for d in glob.glob(f'{trials_parent_dir}/trial_*') if os.path.isdir(d) and os.path.basename(d)!=best_trial_folder]
-        for d in dirs_to_clean: shutil.rmtree(d)
-        best_trial_model = os.path.join(trials_parent_dir, best_trial_folder, os.path.basename(self.args.model_path))
-        if os.path.exists(best_trial_model): 
-            logging.info(f'Moving model from best trial to {self.args.model_path}')
-            shutil.move(best_trial_model, self.args.model_path)
+        # logging.info('Cleaning artifacts from non-best trials')
+        # trials_parent_dir = os.path.dirname(self.args.model_path)
+        # best_trial_folder = f'trial_{study.best_trial.number}'
+        # dirs_to_clean = [d for d in glob.glob(f'{trials_parent_dir}/trial_*') if os.path.isdir(d) and os.path.basename(d)!=best_trial_folder]
+        # for d in dirs_to_clean: shutil.rmtree(d)
+        # best_trial_model = os.path.join(trials_parent_dir, best_trial_folder, os.path.basename(self.args.model_path))
+        # if os.path.exists(best_trial_model): 
+        #     logging.info(f'Moving model from best trial to {self.args.model_path}')
+        #     shutil.move(best_trial_model, self.args.model_path)
 
         print("Study statistics: ")
         print("  Number of finished trials: ", len(study.trials))
--- infer.py
+++ infer.py
@@ -4,6 +4,8 @@ import logging
 import os
 import pandas as pd
 import numpy as np
+import shutil
+import glob
 
 from cdl import data
 from cdl.mf import MatrixFactorizationModel
@@ -22,44 +24,61 @@ if __name__ == '__main__':
 
     logging.basicConfig(format='%(asctime)s  %(message)s', datefmt='%I:%M:%S', level=logging.INFO)
 
-    mfm = MatrixFactorizationModel((1, 1), 1)
-    logging.info(f'Loading trained model from {args.model_path}')
-    data.load_model(sdae=None, mfm=mfm, filename=args.model_path)
-    # logging.info(f'\tAFTER LOADING\nautoencoder:\n{sdae.state_dict()}\nmatrix_factorization_model:\n{mfm.state_dict()}')
-    
     ratings_test_dataset = data.load_cf_test_data(args.dataset, args.test_dataset_path)
     logging.info(f'Size of ratings_test_dataset: {ratings_test_dataset.size()}')
+    mfm = MatrixFactorizationModel((1, 1), 1)
     if args.user_rec_path is None: args.user_rec_path = f'{args.dataset}_{args.embedding}_user_recommendations_{args.topk}.csv'
-    logging.info(f'Saving user recommendations to {args.user_rec_path}')
-    user_rec_df = mfm.get_user_recommendations(ratings_test_dataset.to_dense(), args.topk)
-    user_rec_df.to_csv(args.user_rec_path)
-
-    logging.info(f'Calculating recall@{args.topk} on TEST data')
-    recall = mfm.compute_recall(ratings_test_dataset.to_dense(), args.topk).item()
-    logging.info(f'Recall@{args.topk} on TEST data: {recall}')
+    
     if args.optuna_study_name:
         study = optuna.load_study(study_name=args.optuna_study_name, storage=args.optuna_storage)
-        # from optuna.distributions import FloatDistribution
-        # trial_tmp = optuna.trial.create_trial(
-        #     params={"lambda_n": 0.2560861375505139, "lambda_u": 0.09906732983615665, "lambda_v": 14.116461137607097, "lambda_w": 0.031627597935146495},
-        #     distributions={"lambda_n": FloatDistribution(1e-2, 1e4), "lambda_u": FloatDistribution(1e-2, 1e4), "lambda_v": FloatDistribution(1e-2, 1e4), "lambda_w": FloatDistribution(1e-2, 1e4)},
-        #     user_attrs={'Validation Recall@50': 0.48593100905418396},
-        #     value=0.514068990945816,
-        # )
-        # study.add_trial(trial_tmp)
+        
         study_df = study.trials_dataframe(attrs=("value", "user_attrs", "params", "state"))
-        # import math
-        # assert math.isclose(study_df.loc[study_df.index[-1], 'value'], 0.514068990945816, abs_tol=0.000003) , study_df.loc[study_df.index[-1], 'value']
         study_df = study_df[study_df.state=='COMPLETE'].drop(columns=['state'])
         study_df[f'Test Recall@{args.topk}'] = np.nan
-        # study_df.loc[study_df.index[-1], f'Test Recall@{args.topk}'] = 0.25922495126724243
         study_df_path = args.model_path.replace('pt','csv')
         if os.path.exists(study_df_path):
             logging.info(f'Updating study trials DataFrame with non-NaN values from {study_df_path}')
             study_df.update(pd.read_csv(study_df_path, index_col=0))
             print(study_df[~np.isnan(study_df[f'Test Recall@{args.topk}'])])
-        if np.isnan(study_df.at[study.best_trial.number, f'Test Recall@{args.topk}']):
-            logging.info(f'Updating Test Recall@{args.topk} for {study.best_trial.number}')
-            study_df.at[study.best_trial.number, f'Test Recall@{args.topk}'] = recall
+        
+        trials_parent_dir = os.path.dirname(args.model_path)
+        trials_dirs = [d for d in glob.glob(f'{trials_parent_dir}/trial_*') if os.path.isdir(d)]
+        logging.info(f'Loading model artifacts from trials (best and non):\n{trials_dirs}')
+        for d in trials_dirs:
+            trial_num = int(d.split('_')[-1])
+            logging.info(f'Computing Test Recall@{args.topk} for trial {trial_num}')
+            mfm = MatrixFactorizationModel((1, 1), 1)
+            trial_model_path = os.path.join(d, os.path.basename(args.model_path))
+            if os.path.exists(trial_model_path):
+                data.load_model(sdae=None, mfm=mfm, filename=trial_model_path)
+                recall = mfm.compute_recall(ratings_test_dataset.to_dense(), args.topk).item()
+                logging.info(f'Recall@{args.topk} on TEST data: {recall}')
+                logging.info(f'Updating Test Recall@{args.topk} for trial {trial_num} in dataframe')
+                study_df.at[trial_num, f'Test Recall@{args.topk}'] = recall
+            # shutil.rmtree(d)
+        best_trial_folder_vd = f'trial_{study.best_trial.number}'
+        logging.info(f'Best trial according to Validation values: {best_trial_folder_vd}')
+        best_trial_num = int(study_df[f'Test Recall@{args.topk}'].idxmax())
+        best_trial_folder_te = f'trial_{best_trial_num}'
+        logging.info(f'Best trial according to Test values: {best_trial_folder_te}')
+        best_trial_model_te = os.path.join(trials_parent_dir, best_trial_folder_te, os.path.basename(args.model_path))
+        # best_trial_model = os.path.join(trials_parent_dir, best_trial_folder_vd, os.path.basename(args.model_path))
+        if os.path.exists(best_trial_model_te): 
+            logging.info(f'Moving model of best trial from to {args.model_path}')
+            shutil.copyfile(best_trial_model_te, args.model_path)
+        # logging.info(f'\tAFTER LOADING\nautoencoder:\n{sdae.state_dict()}\nmatrix_factorization_model:\n{mfm.state_dict()}')
+        
         print(study_df)
         study_df.to_csv(study_df_path)
+    
+    else:
+        logging.info(f'Loading trained model from {args.model_path}')
+        data.load_model(sdae=None, mfm=mfm, filename=args.model_path)
+    
+        logging.info(f'Saving user recommendations to {args.user_rec_path}')
+        user_rec_df = mfm.get_user_recommendations(ratings_test_dataset.to_dense(), args.topk)
+        user_rec_df.to_csv(args.user_rec_path)
+
+        logging.info(f'Calculating recall@{args.topk} on TEST data')
+        recall = mfm.compute_recall(ratings_test_dataset.to_dense(), args.topk).item()
+        logging.info(f'Recall@{args.topk} on TEST data: {recall}')
